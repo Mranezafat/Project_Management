c1722bad5de2f34f2509b613e62f8fe3b24b3341
Mranezafat <nezafat.arminn@gmail.com>
2025-07-11 17:35:53 +0330

    remove pass field

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,5 @@ class Profile(models.Model):
     date_joined = models.DateTimeField(auto_now_add=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-    password = models.CharField(max_length=128)  # Add this line
     def __str__(self):
-        return self.user
+        return f"Profile of {self.user.username}" 
